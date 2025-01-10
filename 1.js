c2d309c14d340b78a77460b03724865a8250584e
Jakia123sultana <jakiasultanaania@gmail.com>
2025-01-10 20:59:42 +0600

    added completed project

---
+++ 1.js
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <h3>Mary</h3>
+        <ul>
+            <li><a href="">Protfolio</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Hire Me</a></li>
+        </ul>
+    </nav>
+</header>
